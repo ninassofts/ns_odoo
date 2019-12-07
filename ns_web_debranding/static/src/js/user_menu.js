59d360773a6765ad26c31774fb60f9a4401c7a5c
ninassofts <54321278+ninassofts@users.noreply.github.com>
2019-12-07 19:17:36 +0100

    Update user_menu.js

--- ns_web_debranding/static/src/js/user_menu.js
+++ ns_web_debranding/static/src/js/user_menu.js
@@ -1,7 +1,7 @@
-odoo.define('web_debranding.UserMenu', function (require) {
+odoo.define('ns_web_debranding.UserMenu', function (require) {
     "use strict";
 
-    require('web_debranding.base');
+    require('ns_web_debranding.base');
     var session = require('web.session');
     var core = require('web.core');
     var _t = core._t;
